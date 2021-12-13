1c28b30a13e31e91e0f13f601407581e7f0d10b3
Jader <jader.moura.gg@gmail.com>
2021-12-13 15:38:18 -0300

    solid implemented

---
+++ src/repositories/PostgresCategoryRepository.ts
@@ -0,0 +1,18 @@
+import { Category } from "../model/Category";
+import { ICategoriesRepository } from "./ICategoriesRepository";
+
+class PostgresCategoryRepository implements ICategoriesRepository {
+    findByName(name: string): Category {
+        throw new Error("Method not implemented.");
+    }
+    list(): Category[] {
+        throw new Error("Method not implemented.");
+    }
+    create(name: string, description: string): void {
+        throw new Error("Method not implemented.");
+    }
+
+
+}
+
+export { PostgresCategoryRepository }
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,26 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository){
+
+    }
+
+    execute({ description, name } : IRequest):void {
+
+        const categoryAlreadyExist = this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExist){
+            throw new Error("Category already exists!")
+        }
+
+        this.categoriesRepository.create({name, description});
+    }
+}
+
+export { CreateCategoryService };
