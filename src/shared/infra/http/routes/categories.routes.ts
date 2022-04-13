ed99c2930737584d5bb43833e92d1236b760943b
jader-moura <jader.moura@monitoratec.com.br>
2022-04-13 20:44:11 -0300

    ensure admin implemented

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -3,6 +3,8 @@ import { ImportCategoryController } from "@modules/cars/useCases/importCategory/
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 import { Router } from "express";
 import multer from "multer";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 
 const categoriesRoutes = Router();
@@ -14,11 +16,25 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+    "/",  
+    ensureAuthenticated, 
+    ensureAdmin, 
+    createCategoryController.handle
+);
 
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get(
+    "/", 
+    listCategoriesController.handle
+);
 
-categoriesRoutes.post("/import", upload.single("file"), importCategoryController.handle)
+categoriesRoutes.post(
+    "/import",  
+    ensureAuthenticated, 
+    ensureAdmin, 
+    upload.single("file"), 
+    importCategoryController.handle
+)
 
 
 
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationsController } from '../../../../modules/cars/useCases/createSpecifications/CreateSpecificationsController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const specificationsRoutes = Router();
 
 const createSpecificationsController = new CreateSpecificationsController();
 
 specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationsController.handle);
+specificationsRoutes.post(
+    "/",    
+    ensureAuthenticated, 
+    ensureAdmin,  
+    createSpecificationsController.handle
+);
 
 export { specificationsRoutes };
